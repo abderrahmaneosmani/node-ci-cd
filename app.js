af24fb9285ee8aadabbda7a992d53c58156a57a9
abderrahmane <abderrahmaneosmani2@gmail.com>
2022-08-29 13:19:34 +0100

    update

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import express from "express";
 import cors from "cors";
+import { path } from "path";
 const PORT = 5000;
 const app = express();
 
+app.use(express.static(path.join(__dirname(), "/frontend/build")));
 app.use(cors());
 
 app.get("/name", (req, res) => {
